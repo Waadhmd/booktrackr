51ea4c799994b7b19f08727c212033821d367605
waadhmd <waad.hamoud94@gmail.com>
2025-10-01 14:19:11 +0200

    feat: add /add_book router with db commit functionality

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 
 # Needed for flash()
 app.secret_key = "supersecretkey123"
-# Now initialize the db with the app
+# initialize the db with the app
 db.init_app(app)
 
 @app.route('/add_author',methods=['GET','POST'])
@@ -51,7 +51,35 @@ def list_authors():
     authors = Author.query.all()
     return render_template('authors.html', authors=authors)
 
-#with app.app_context():
+@app.route('/add_book',methods=['GET','POST'])
+def add_book():
+    authors = Author.query.all()
+    success = None
+    if request.method == 'POST':
+        title = request.form.get('title')
+        isbn = request.form.get('isbn')
+        publication_year_str = request.form.get('publication_year')
+        author_id = request.form.get('author_id')
+        publication_year = None
+        if publication_year_str:
+            try:
+                publication_year = int(publication_year_str)
+            except ValueError:
+                publication_year = None
+
+        if title and isbn and author_id:
+            book = Book(
+                title=title,
+                isbn=isbn,
+                publication_year=publication_year,
+                author_id=author_id,
+            )
+            db.session.add(book)
+            db.session.commit()
+            success = f"Book {title} added successfully."
+    return render_template('add_book.html',authors=authors,success=success)
+
+ #with app.app_context():
  #   db.create_all()
 if __name__ == '__main__':
     app.run(debug=True)
